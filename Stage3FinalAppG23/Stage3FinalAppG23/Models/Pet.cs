580f88c6d25707563752ce1fc3e306783be73833
ismail203013 <78367195+ismail203013@users.noreply.github.com>
2021-04-24 16:30:04 +0100

    PET AND PRODUCTS

---
+++ Stage3FinalAppG23/Stage3FinalAppG23/Models/Pet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stage3FinalAppG23.Models
+{
+    public class Pet
+    {
+        public string Name { get; set; }
+        public string DOB { get; set; }
+    }
+}
---
+++ Stage3FinalAppG23/Stage3FinalAppG23/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stage3FinalAppG23.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string Description { get; set; }
+        public string FilterType { get; set; }
+        public string File { get; set; }
+        public string LogoFile { get; set; }
+    }
+}
