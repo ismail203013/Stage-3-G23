bd89c6d44b0bbaef9844001147d4b12bf0711b15
harrybuxton36 <75274006+harrybuxton36@users.noreply.github.com>
2021-04-24 16:42:35 +0100

    connection string

---
+++ Stage3FinalAppG23/Stage3FinalAppG23/Pages/Database/ConnectionString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stage3FinalAppG23.Pages.Database
+{
+    public class ConnectionString
+    {
+        private string connection;
+
+        public string DatabaseConn()
+        {
+            connection = @"";
+            return connection;
+        }
+    }
+}
