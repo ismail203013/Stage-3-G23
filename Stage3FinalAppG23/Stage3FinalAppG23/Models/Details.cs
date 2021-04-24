25ffc5f3a5a7a31f01a8a879b2798f7ca820cdc3
harrybuxton36 <75274006+harrybuxton36@users.noreply.github.com>
2021-04-24 16:24:59 +0100

    Details

---
+++ Stage3FinalAppG23/Stage3FinalAppG23/Models/Details.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stage3FinalAppG23.Models
+{
+    public class Details
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string redoEmail { get; set; }
+        public string phoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Apartment { get; set; }
+        public string zipcode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string state { get; set; }
+    }
+}
