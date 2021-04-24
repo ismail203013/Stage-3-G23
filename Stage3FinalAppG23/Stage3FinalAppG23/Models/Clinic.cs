b79d8a5583a4de86fc5c4bc34af7415304bb0b52
Ahmed-20-02 <c0019714@my.shu.ac.uk>
2021-04-24 16:24:00 +0100

    Created Clinic Class

---
+++ Stage3FinalAppG23/Stage3FinalAppG23/Models/Clinic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stage3FinalAppG23.Models
+{
+    public class Clinic
+    {
+        public string Name { get; set; }
+        public string country { get; set; }
+        public string state { get; set; }
+        public string Address { get; set; }
+        public string zipcode { get; set; }
+    }
+}
