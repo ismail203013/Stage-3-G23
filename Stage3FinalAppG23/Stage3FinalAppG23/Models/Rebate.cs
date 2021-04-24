40e2f61cba632bca376428e6d10f4d61fe1b714b
Amaan-Nazir <78367439+Amaan-Nazir@users.noreply.github.com>
2021-04-24 16:34:09 +0100

    Pet and User classes

---
+++ Stage3FinalAppG23/Stage3FinalAppG23/Models/Rebate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace Stage3FinalAppG23.Models
+{
+    public class Rebate
+    {
+        [Required]
+        [Display(Name = "Rebate Selection")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public void Rebate1()
+        {
+            Name = "Receive Card Via Mail";
+            Description = "Receive A Physical Pre-Paid Mastercard Via Mail Delivery Within 6-8 Weeks After Valid Submission";
+        }
+        public void Rebate2()
+        {
+            Name = "Receive Card Via Email";
+            Description = "Receive A Virtual Pre-Paid Mastercard" +
+                "Via Email Delivery Within 2 Weeks After Valid Submission";
+        }
+    }
+}
---
+++ Stage3FinalAppG23/Stage3FinalAppG23/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stage3FinalAppG23.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public string redoPassword { get; set; }
+        public bool UserOffers { get; set; }
+        public string PhoneNumber { get; set; }
+        public string StreetAddress { get; set; }
+        public string Apartment { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+
+        public string Pet1Name { get; set; }
+        public string Pet1Birthday { get; set; }
+        public string Pet2Name { get; set; }
+        public string Pet2Birthday { get; set; }
+        public string Pet3Name { get; set; }
+        public string Pet3Birthday { get; set; }
+    }
+}
