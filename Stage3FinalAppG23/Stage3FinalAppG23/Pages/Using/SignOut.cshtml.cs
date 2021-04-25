cc6de99f7a246efedd502ed7ea58cf9c1d040450
harrybuxton36 <75274006+harrybuxton36@users.noreply.github.com>
2021-04-25 18:07:25 +0100

    signout c#

--- Stage3FinalAppG23/Stage3FinalAppG23/Pages/Using/SignOut.cshtml.cs
+++ Stage3FinalAppG23/Stage3FinalAppG23/Pages/Using/SignOut.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,18 @@ namespace Stage3FinalAppG23.Pages.Using
 {
     public class SignOutModel : PageModel
     {
-        public void OnGet()
+        public string Message;
+
+        public string ID { get; set; }
+
+        public IActionResult OnGet()
         {
+            ID = HttpContext.Session.GetString("ID");
+
+            HttpContext.Session.Clear();
+            Message = "You have been successfully signed out.";
+
+            return Page();
         }
     }
 }
